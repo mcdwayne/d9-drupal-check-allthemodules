b99ffd1e2139460e7f7c6331e8c3db75a9258003
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 00:45:10 -0700

    added modulename to this branch

---
+++ web/modules/contrib/admin_menu/lib/Drupal/admin_menu/AdminMenuBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\admin_menu;
+
+use Drupal\Core\Cache\CacheFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Admin menu dependency injection container.
+ */
+class AdminMenuBundle extends Bundle {
+
+  /**
+   * Overrides Symfony\Component\HttpKernel\Bundle\Bundle::build().
+   */
+  public function build(ContainerBuilder $container) {
+    CacheFactory::registerBin($container, 'admin_menu');
+  }
+
+}
