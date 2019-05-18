d58f5fd2b54017db6b10b5a9b016e678396a673f
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 15:47:05 -0700

    added modulename to this branch

---
+++ web/modules/contrib/replicate/src/Events/ReplicateAlterEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\replicate\Events;
+
+use Drupal\Core\Entity\EntityInterface;
+
+class ReplicateAlterEvent extends ReplicateEventBase {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityInterface
+   */
+  protected $original;
+
+  public function __construct(EntityInterface $entity, EntityInterface $original) {
+    parent::__construct($entity);
+    $this->original = $original;
+  }
+
+  /**
+   * @return \Drupal\Core\Entity\EntityInterface
+   */
+  public function getOriginal() {
+    return $this->original;
+  }
+
+}
