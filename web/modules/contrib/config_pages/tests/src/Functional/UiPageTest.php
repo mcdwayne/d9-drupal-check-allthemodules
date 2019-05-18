83491652ba489d90a682bdcc9c2e29336992a28c
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 04:36:07 -0700

    added modulename to this branch

---
+++ web/modules/contrib/config_pages/tests/src/Functional/UiPageTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\config_pages\tests\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+* Tests that the ConfigPages UI pages are reachable.
+*
+* @group config_pages
+*/
+class UiPageTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['config_pages'];
+
+  /**
+   * Tests that the ConfigPages listing page works.
+   */
+  public function testConfigPagesPage() {
+    $account = $this->drupalCreateUser(['access config_pages overview']);
+    $this->drupalLogin($account);
+
+    $this->drupalGet('admin/structure/config_pages');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Test that there is an empty reaction rule listing.
+    $this->assertSession()->pageTextContains('There is no Config page yet.');
+
+  }
+
+  /**
+   * Tests that the ConfigPages types listing page works.
+   */
+  public function testConfigPagesTypesPage() {
+    $account = $this->drupalCreateUser(['administer config_pages types']);
+    $this->drupalLogin($account);
+
+    $this->drupalGet('admin/structure/config_pages/types');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Test that there is an empty reaction rule listing.
+    $this->assertSession()->pageTextContains('There is no Config page type yet.');
+  }
+
+}
