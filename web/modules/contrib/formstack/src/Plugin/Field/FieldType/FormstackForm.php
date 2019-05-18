6927bd08a02ac87c0953909776932f3c772591a8
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 08:46:07 -0700

    added modulename to this branch

---
+++ web/modules/contrib/formstack/src/Plugin/Field/FieldType/FormstackForm.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\formstack\Plugin\Field\FieldType;
+
+use Drupal\Component\Utility\Random;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'formstack_form' field type.
+ *
+ * @FieldType(
+ *   id = "formstack_form",
+ *   label = @Translation("Formstack form"),
+ *   description = @Translation("Display a Formstack form"),
+ *   default_widget = "formstack_form_widget",
+ *   default_formatter = "formstack_form_formatter"
+ * )
+ */
+class FormstackForm extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultStorageSettings() {
+    return [
+      'max_length' => 255,
+      'is_ascii' => FALSE,
+      'case_sensitive' => FALSE,
+    ] + parent::defaultStorageSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    // Prevent early t() calls by using the TranslatableMarkup.
+    $properties['formstack_id'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Text value'))
+      ->setSetting('case_sensitive', $field_definition->getSetting('case_sensitive'))
+      ->setRequired(TRUE);
+
+    $properties['settings'] = DataDefinition::create('any')
+      ->setLabel(new TranslatableMarkup('Settings'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = [
+      'columns' => [
+        'formstack_id' => [
+          'type' => $field_definition->getSetting('is_ascii') === TRUE ? 'varchar_ascii' : 'varchar',
+          'length' => (int) $field_definition->getSetting('max_length'),
+          'binary' => $field_definition->getSetting('case_sensitive'),
+        ],
+        'settings' => [
+          'type' => 'blob',
+          'serialized' => TRUE,
+        ],
+      ],
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints() {
+    $constraints = parent::getConstraints();
+
+    return $constraints;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
+    $random = new Random();
+    $values['formstack_id'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
+    $elements = [];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('formstack_id')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+}
