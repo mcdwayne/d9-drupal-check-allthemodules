80e92488b0dde00b7c0d4984bbc1d3ba00f9e1e3
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 00:39:14 -0700

    added modulename to this branch

---
+++ web/modules/contrib/address_map_link/src/MapLinkInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\address_map_link;
+
+use Drupal\address\AddressInterface;
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+/**
+ * Defines an interface for Map link plugins.
+ */
+interface MapLinkInterface extends PluginInspectionInterface {
+
+  /**
+   * Return the name of the map link plugin.
+   *
+   * @return string
+   *   The name of the MapLink plugin.
+   */
+  public function getName();
+
+  /**
+   * Gets the map link url from an address.
+   *
+   * @param \Drupal\address\AddressInterface $address
+   *   The address.
+   *
+   * @return \Drupal\Core\Url
+   *   The Url.
+   */
+  public function getAddressUrl(AddressInterface $address);
+
+}
