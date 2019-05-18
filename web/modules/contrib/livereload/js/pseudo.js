524c2e1edf93178207eb51e019d324c51d69512f
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 11:33:00 -0700

    added modulename to this branch

---
+++ web/modules/contrib/livereload/js/pseudo.js
@@ -0,0 +1,7 @@
+/**
+ * @file
+ * Pseudo library file for the libraries module.
+ *
+ * Since it is not possible yet to define a dynamic library path, we need to provide a pseudo script for the validator,
+ * which we will then change by our dynamic path via hook_library_info_alter().
+ */
