e3d341081b369a75f67c2f6c036d438d9b2caddd
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 03:12:26 -0700

    added modulename to this branch

---
+++ web/modules/contrib/cloudmenu/lib/Drupal/cloudmenu/Form/SettingsForm.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\cloudmenu\Form\SettingsForm.
+ */
+
+namespace Drupal\cloudmenu\Form;
+
+use Drupal\system\SystemConfigFormBase;
+use Drupal\Core\Config\ConfigFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configures aggregator settings for this site.
+ */
+class SettingsForm extends SystemConfigFormBase {
+
+}
