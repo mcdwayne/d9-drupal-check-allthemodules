726b27628d950b6dd7c7838b83eebbe54331cf2a
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 15:42:39 -0700

    added modulename to this branch

---
+++ web/modules/contrib/rekognition_api/README.md
@@ -0,0 +1,35 @@
+# Rekognition API
+
+This module interacts with the [AWS Rekognition](https://aws.amazon.com/rekognition/) service to identify objects and faces in photos. The module requires integration with an active [Amazon Web Services](https://aws.amazon.com) (AWS) account, and also requires some initial setup in order to use with a Drupal site using the [Media Entity](https://www.drupal.org/project/media_entity) and [Media Entity Image](https://www.drupal.org/project/media_entity_image) modules.
+
+## Requirements
+
+This module assumes you have configured a few entity types:
+
+  - Taxonomy 'Name' (`name`)
+  - Taxonomy 'Face' (`face`)
+    - Field 'Name' (`field_name`) - entity reference to Name taxonomy.
+  - Taxonomy 'Label' (`label`)
+  - Media Image 'Image' (`image`)
+    - Field 'Image' (`field_image`) that stores images in S3 (via the [S3 File System](https://www.drupal.org/project/s3fs) module).
+    - Field 'Label' (`field_label`) - entity reference to Label taxonomy.
+    - Field 'Face' (`field_face`) - entity reference to Face taxonomy.
+
+Also, you must be using the [S3 File System](https://www.drupal.org/project/s3fs) module to store all Media Image images on Amazon S3 in a specific bucket.
+
+## AWS Setup - S3, Lambda, and Rekognition
+
+This module includes an AWS CloudFormation stack template (inside the `lambda` folder) for quick and automatic set up of all the required resources and permissions to integrate S3, Lambda, and Rekognition so they work with your Drupal site.
+
+See the detailed guide that's part of the [Drupal Photo Gallery](https://github.com/geerlingguy/drupal-photo-gallery) documentation for more details on how to use the AWS CLI to deploy this CloudFormation stack inside your own account.
+
+## Authors
+
+This project was initially created during Acquia's 2017 Build Week Hackathon by:
+
+  - [Matthew Wagner](https://ma.ttwagner.com)
+  - [Glenn Pratt](https://about.me/glennpratt)
+  - [ASH Heath](http://www.burnashburn.com)
+  - [Jeff Geerling](https://www.jeffgeerling.com)
+  - [Rok Zlender](https://twitter.com/Zlender)
+  - [Meagan White](https://twitter.com/MeaganWhite_)
---
+++ web/modules/contrib/rekognition_api/lambda/index.js
@@ -0,0 +1,135 @@
+// Lambda function (for the nodejs6.10 runtime) which does the following:
+//
+//   - Receives an event call when an image is stored in an S3 bucket.
+//   - Detects Labels in said image via Rekognition.
+//     - If any are found, send POST with data to Drupal site (DRUPAL_URL).
+//   - Detects Faces in said image via Rekognition.
+//     - If any are found, send POST with data to Drupal site (DRUPAL_URL).
+
+'use strict';
+
+const aws = require('aws-sdk');
+const http = require('http');
+const drupal_url = process.env.DRUPAL_URL;
+const object_endpoint = "/rekognition_api/objects/create?_format=json";
+const drupal_auth = "Basic " + new Buffer(process.env.DRUPAL_USERNAME + ":" + process.env.DRUPAL_PASSWORD).toString("base64");
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+var rekognition = new aws.Rekognition({apiVersion: '2016-06-27'});
+
+exports.handler = (event, context, callback) => {
+    var body='';
+
+    // Get the object from the event and detect Labels on it
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Image: {
+            S3Object: {
+                Bucket: bucket,
+                Name: key,
+            },
+        },
+        MaxLabels: 30,
+        MinConfidence: 30,
+    };
+
+    // Detect Labels.
+    rekognition.detectLabels(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred.
+        else {
+            var jsonObject = {
+                Bucket: bucket,
+                Name: key,
+                Labels: data.Labels
+            }
+            var body = JSON.stringify(jsonObject);
+
+            // POST options.
+            var options_post = {
+                host: drupal_url,
+                path: object_endpoint,
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": Buffer.byteLength(body),
+                    "Authorization": drupal_auth,
+                }
+            };
+
+            var reqPost = http.request(options_post, function(res) {
+                console.log("label POST request statusCode: ", res.statusCode);
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+            });
+
+            // console.log("labels: ", jsonObject);
+            setTimeout(function() {
+              reqPost.write(body);
+              reqPost.end();
+            }, 4000);
+        }
+    });
+    delete params.MinConfidence
+    delete params.MaxLabels
+
+    // Detect Faces.
+    params["CollectionId"] = process.env.COLLECTION
+    var response_faces = [];
+    rekognition.indexFaces(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else {
+            data.FaceRecords.forEach(function(face) {
+                var search_params = {
+                    CollectionId: process.env.COLLECTION,
+                    FaceId: face.Face.FaceId,
+                    FaceMatchThreshold: 90,
+                    MaxFaces: 10
+                };
+                rekognition.searchFaces(search_params, function(err2, data2) {
+                    if (err) console.log(err2, err2.stack);
+                    else {
+                        // console.log(data2);
+                        // console.log(data2.FaceMatches.length);
+                        if (data2.FaceMatches.length > 0) {
+                            face.FaceMatches = data2.FaceMatches;
+                            // console.log(face);
+                        }
+                        response_faces.push(face);
+                        var jsonFacesObject = {
+                            Bucket: bucket,
+                            Name: key,
+                            Faces: response_faces
+                        }
+                        var body = JSON.stringify(jsonFacesObject);
+
+                        // console.log(JSON.stringify(response_faces));
+                        // the POST options
+                        var options_post = {
+                            host: drupal_url,
+                            path: object_endpoint,
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Content-Length": Buffer.byteLength(body),
+                                "Authorization": drupal_auth,
+                            }
+                        };
+
+                        var reqPost = http.request(options_post, function(res) {
+                            console.log("face POST request statusCode: ", res.statusCode);
+                            res.on('data', function (chunk) {
+                                body += chunk;
+                            });
+                        });
+
+                        setTimeout(function() {
+                            reqPost.write(body);
+                            reqPost.end();
+                        }, 5000);
+                    }
+                });
+            });
+        }
+    });
+};
