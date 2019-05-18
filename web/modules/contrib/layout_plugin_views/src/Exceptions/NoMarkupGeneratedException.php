d30f79f6a77e18b4350a2c7e742017975f95c21e
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 11:07:46 -0700

    added modulename to this branch

---
+++ web/modules/contrib/layout_plugin_views/src/Exceptions/NoMarkupGeneratedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\layout_plugin_views\Exceptions;
+
+/**
+ * Exception which can be thrown if a render function did not generate markup.
+ */
+class NoMarkupGeneratedException extends \RuntimeException {
+
+}
