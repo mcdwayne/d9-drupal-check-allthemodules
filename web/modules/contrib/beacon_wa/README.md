4ec3355d40cd7d25d26ece1b92c6d68cca188c17
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 01:40:40 -0700

    added modulename to this branch

---
+++ web/modules/contrib/beacon_wa/README.md
@@ -0,0 +1,6 @@
+This module provides integration with the Beacon social media web tracker
+
+## Features
+
+Installs a piece of Javascript on to every page load in the footer apart from default admin pages.
+
---
+++ web/modules/contrib/beacon_wa/js/beacon.js
@@ -0,0 +1,4 @@
+(function(a, d, w){
+    var h= d.getElementsByTagName(a[0])[0]; var s= d.createElement(a[1]);
+    s.setAttribute("type", a[2]); s.setAttribute("src", a[3]); s.setAttribute(a[4], true); h.appendChild(s);
+})(['head', 'script', 'text/javascript', '//tracker.thisisbeacon.com/tracker/', 'async'], document, window);
