ba22712ef624008907f9733d246636575e55ce0f
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 06:27:03 -0700

    added modulename to this branch

---
+++ web/modules/contrib/drush_entity_tools/scripts/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/robballou/drush_typeinfo/8.x-1.x/typeinfo.drush.inc > typeinfo.drush.inc
+curl https://raw.githubusercontent.com/robballou/drush_taxonomyinfo/8.x-1.x/taxonomyinfo.drush.inc > taxonomyinfo.drush.inc
