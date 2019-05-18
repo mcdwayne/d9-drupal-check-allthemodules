fe464fc6348191d3b70d2f3bd5b63e276e302a35
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 05:24:35 -0700

    added modulename to this branch

---
+++ web/modules/contrib/cyrillic_to_latin/README.md
@@ -0,0 +1,53 @@
+# CYRILLIC TO LATIN
+
+## CONTENTS OF THIS FILE
+
+  * Introduction
+  * Requirements
+  * Installation
+  * Configuration
+  * Author
+
+## INTRODUCTION
+
+Cyrillic to Latin module will convert strings that are passed through Drupal t() 
+function and string/text field values. If you are using the Address module, 
+country name will also be converted to latin.
+
+## REQUIREMENTS
+
+No special requirements.
+
+## INSTALLATION
+
+Use Composer to install the module:
+
+```
+composer require drupal/cyrillic_to_latin
+```
+
+and then enable it with Drush:
+
+```
+drush en cyrillic_to_latin -y
+```
+
+## CONFIGURATION
+
+You can enable/disable string conversion here:
+
+```
+'/admin/config/regional/cyrillic-to-latin'
+```
+
+### AUTHOR
+
+Goran Nikolovski  
+Website: http://www.gorannikolovski.com  
+Drupal.org: https://www.drupal.org/u/gnikolovski  
+Email: nikolovski84@gmail.com  
+
+Company: Studio Present, Subotica, Serbia  
+Website: http://www.studiopresent.com  
+Drupal: https://www.drupal.org/studio-present  
+Email: info@studiopresent.com  
---
+++ web/modules/contrib/cyrillic_to_latin/src/CyrillicToLatinManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\cyrillic_to_latin;
+
+use Drupal\Core\StringTranslation\TranslationManager;
+
+/**
+ * Defines a chained translation implementation combining multiple translators.
+ */
+class CyrillicToLatinManager extends TranslationManager {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function doTranslate($string, array $options = []) {
+    // If a NULL langcode has been provided, unset it.
+    if (!isset($options['langcode']) && array_key_exists('langcode', $options)) {
+      unset($options['langcode']);
+    }
+
+    // Merge in options defaults.
+    $options = $options + [
+      'langcode' => $this->defaultLangcode,
+      'context' => '',
+    ];
+    $translation = $this->getStringTranslation($options['langcode'], $string, $options['context']);
+
+    $config = \Drupal::configFactory()->get('cyrillic_to_latin.settings');
+    if ($config->get('enabled')) {
+      return $translation === FALSE ? $string : self::convertCyrillicToLatin($translation);
+    }
+
+    return $translation === FALSE ? $string : $translation;
+  }
+
+  /**
+   * Converts cyrillic letters to latin.
+   *
+   * @param string $string
+   *   A string containing the text to convert.
+   *
+   * @return string
+   *   The converted string.
+   */
+  public static function convertCyrillicToLatin($string) {
+    $cyrillic = [
+      'њ', 'љ', 'а', 'б', 'в', 'г', 'д', 'ђ', 'e', 'ж', 'з', 'и', 'ј', 'к', 'л',
+      'м', 'н', 'о', 'п', 'р', 'с', 'т', 'ћ', 'у', 'ф', 'х', 'ц', 'ч', 'џ', 'ш',
+      'Њ', 'Љ', 'А', 'Б', 'В', 'Г', 'Д', 'Ђ', 'Е', 'Ж', 'З', 'И', 'Ј', 'К', 'Л',
+      'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'Ћ', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Џ', 'Ш',
+    ];
+
+    $latin = [
+      'nj', 'lj', 'a', 'b', 'v', 'g', 'd', 'đ', 'e', 'ž', 'z', 'i', 'j', 'k',
+      'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'ć', 'u', 'f', 'h', 'c', 'č',
+      'dž', 'š', 'Nj', 'Lj', 'A', 'B', 'V', 'G', 'D', 'Đ', 'E', 'Ž', 'Z', 'I',
+      'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'Ć', 'U', 'F', 'H', 'C',
+      'Č', 'Dž', 'Š',
+    ];
+
+    return str_replace($cyrillic, $latin, $string);
+  }
+
+}
