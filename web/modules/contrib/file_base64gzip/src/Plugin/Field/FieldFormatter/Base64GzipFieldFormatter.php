a993d6dcbf9d63f36cded9d0e699066d4518ad3b
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 08:15:24 -0700

    added modulename to this branch

---
+++ web/modules/contrib/file_base64gzip/src/Plugin/Field/FieldFormatter/Base64GzipFieldFormatter.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\file_base64gzip\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'base64gzip_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "base64gzip_field_formatter",
+ *   label = @Translation("Base64gzip field formatter"),
+ *   field_types = {
+ *     "file"
+ *   }
+ * )
+ */
+class Base64GzipFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    $file_uri = $item->entity->getFileUri();
+    return base64_encode(gzencode(implode("", file($file_uri))));
+  }
+
+}
