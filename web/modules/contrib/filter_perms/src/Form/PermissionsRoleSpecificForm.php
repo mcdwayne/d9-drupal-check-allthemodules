ad4f091e075a1f5d4dea7a0a70144bc52546bc2a
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 08:22:59 -0700

    added modulename to this branch

---
+++ web/modules/contrib/filter_perms/src/Form/PermissionsRoleSpecificForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\filter_perms\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\RoleInterface;
+
+/**
+ * Provides the user permissions administration form for a specific role.
+ */
+class PermissionsRoleSpecificForm extends PermissionsForm {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\user\RoleInterface $user_role
+   *   The user role used for this form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, RoleInterface $user_role = NULL) {
+    $this->saveFilterSettings((array) $user_role->id(), (array) self::ALL_OPTIONS);
+    return $this->redirect('user.admin_permissions');
+  }
+
+}
