5184aa01788c4df04408c08a30c6f4f3fb038575
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 12:21:45 -0700

    added modulename to this branch

---
+++ web/modules/contrib/menu_link_content_fields/src/Routing/RouteSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\menu_link_content_fields\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('field_ui.field_storage_config_add_menu_link_content')) {
+      $route->setDefault('bundle', 'menu_link_content');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = parent::getSubscribedEvents();
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -200];
+    return $events;
+  }
+
+}
