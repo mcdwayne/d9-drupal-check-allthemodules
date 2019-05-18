861da0c67a1bf38cdaca3281f5fc904ceec28fb5
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 06:20:26 -0700

    added modulename to this branch

---
+++ web/modules/contrib/pm/pm-list-default.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Template file for pm-list-default.
+ */
+?>
+<div class="pm-list-default-wrapper pm-list-field-name-<?php echo $field_name ?>">
+  <?php echo pm_helper_get_fa_icon($key); ?>&nbsp;<span class="value"><?php echo $value; ?></span>
+</div>
