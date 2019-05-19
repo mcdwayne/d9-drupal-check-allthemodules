3c585f2beb852bed9df9ef50b602f21d81ce739e
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 18:45:02 -0700

    added modulename to this branch

---
+++ web/modules/contrib/term_reference_sortable/js/taxonomy_term_reference_sortable_widget.js
@@ -0,0 +1,68 @@
+(function($) {
+  Drupal.behaviors.termReferenceSortable = {
+    attach: function(context, settings) {
+      //init select
+      $('.sortable2').parents('.field-suffix').siblings('select').hide();
+
+      $('.sortable2').each(function(index, value){
+        $this = $(this);
+        $select = $this.parents('.field-suffix').siblings('select');
+        if($select.hasClass('error')){
+          $this.addClass('error');
+        }
+
+        $select.find('option').each(function(index, value){
+          $option = $(value);
+          if($option.val() != '_none'){
+
+            if($(value).attr('selected')){
+              $this.append('<li class="ui-state-highlight"  id="' + $option.val() + '">' + $option.html() + '</li>');
+            } else {
+              $this.parent().find('.sortable1').append('<li class="ui-state-default"  id="' + $option.val() + '">' + $option.html() + '</li>');
+            }
+          }
+        });
+      });
+
+      $(".sortable1, .sortable2").sortable({
+        connectWith: ".connectedSortable"
+      }).disableSelection();
+
+      $(".sortable2").sortable({
+        receive:function(event, ui){
+          var max_elements = $(event.target).parent('.sortable-lists').attr('max_elem');
+          if(max_elements){
+            if($(event.target).sortable('toArray').length > max_elements){
+              $(ui.sender).sortable('cancel');
+              var $sortable_list  = $(event.target).parents('.sortable-lists');
+              if($sortable_list.find('.error-message').length < 1){
+                $sortable_list.prepend('<div class="error-message">Maximum limit: ' + max_elements + '</div>');
+              }
+            }
+          }
+        }
+      });
+
+      $(".sortable2").bind("sortupdate", function(event, ui) {
+        sortSelect(this);
+      });
+
+      function sortSelect(element){
+        var selected = $(element).sortable('toArray');
+        var $select = $(element).parents('.field-suffix').siblings('select');
+        $select.find(':selected').attr("selected", "");
+        if($(selected).length > 0){
+          $(selected.reverse()).each(function(index, value){
+            var $curent_option = $select.find('option:[value="'+ value +'"]');
+            var $curent_option_clone = $curent_option.clone().attr("selected", "selected");
+            $curent_option.remove();
+            $select.prepend($curent_option_clone);
+
+          });
+        }
+      }
+
+    }
+  };
+
+})(jQuery);
