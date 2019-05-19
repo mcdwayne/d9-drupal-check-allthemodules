ff58850ba9e2cf82de405436111284dc3a8716a6
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 22:16:24 -0700

    added modulename to this branch

---
+++ web/modules/contrib/zurb_responsive_embed/README.txt
@@ -0,0 +1,26 @@
+ZURB Responsive Embed
+
+This module adds a Link Field Formatter for Drupal 8 built upon Zurb Foundation 6 Responsive Embed component.
+
+Included formatters
+---
+By default this modules includes three formatters:
+
+- Zurb Responsive Embed (4:3)
+- Zurb Responsive Embed Panorama (256:81)
+- Zurb Responsive Embed Widescreen (16:9)
+
+Installation
+---
+First of all you have to download the module and place it inside your *module* folder inside your Drupal project. You can do this with Composer:
+
+  composer require drupal/zurb_video_field
+
+
+How it works
+---
+- Add a new field to your content type
+- Choose Link from the Field Type dropdown
+- Customize the settings of this field as you want
+- Under the Content Type display tab choose one of the Zurb Video formatter
+- Have fun!
---
+++ web/modules/contrib/zurb_responsive_embed/src/Plugin/Field/FieldFormatter/ZurbResponsiveEmbedPanoramaFormatter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\zurb_responsive_embed\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'zurb_video_field_panorama_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "zurb_video_field_panorama_formatter",
+ *   label = @Translation("Zurb Responsive Embed Panorama"),
+ *   field_types = {
+ *     "link"
+ *   }
+ * )
+ */
+class ZurbResponsiveEmbedPanoramaFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $url = $item->getUrl() ?: Url::fromRoute('<none>');
+      $elements[$delta] = [
+        '#theme' => 'zurb_responsive_embed_panorama_formatter',
+        '#url' => $url,
+      ];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
