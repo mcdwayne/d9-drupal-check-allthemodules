d38a99e7e2812ca4c835cd2dee1fa4e28fb86a0c
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 17:43:49 -0700

    added modulename to this branch

---
+++ web/modules/contrib/social_auth_google/src/Controller/GoogleAuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\social_auth_google\Controller;
+
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_auth\Controller\OAuth2ControllerBase;
+use Drupal\social_auth\SocialAuthDataHandler;
+use Drupal\social_auth\User\UserAuthenticator;
+use Drupal\social_auth_google\GoogleAuthManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Returns responses for Social Auth Google module routes.
+ */
+class GoogleAuthController extends OAuth2ControllerBase {
+
+  /**
+   * GoogleAuthController constructor.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
+   *   Used to get an instance of social_auth_google network plugin.
+   * @param \Drupal\social_auth\User\UserAuthenticator $user_authenticator
+   *   Used to manage user authentication/registration.
+   * @param \Drupal\social_auth_google\GoogleAuthManager $google_manager
+   *   Used to manage authentication methods.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request
+   *   Used to access GET parameters.
+   * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
+   *   The Social Auth data handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Used to handle metadata for redirection to authentication URL.
+   */
+  public function __construct(MessengerInterface $messenger,
+                              NetworkManager $network_manager,
+                              UserAuthenticator $user_authenticator,
+                              GoogleAuthManager $google_manager,
+                              RequestStack $request,
+                              SocialAuthDataHandler $data_handler,
+                              RendererInterface $renderer) {
+
+    parent::__construct('Social Auth Google', 'social_auth_google',
+                        $messenger, $network_manager, $user_authenticator,
+                        $google_manager, $request, $data_handler, $renderer);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('messenger'),
+      $container->get('plugin.network.manager'),
+      $container->get('social_auth.user_authenticator'),
+      $container->get('social_auth_google.manager'),
+      $container->get('request_stack'),
+      $container->get('social_auth.data_handler'),
+      $container->get('renderer')
+    );
+  }
+
+  /**
+   * Response for path 'user/login/google/callback'.
+   *
+   * Google returns the user here after user has authenticated.
+   */
+  public function callback() {
+
+    // Checks if authentication failed.
+    if ($this->request->getCurrentRequest()->query->has('error')) {
+      $this->messenger->addError($this->t('You could not be authenticated.'));
+
+      return $this->redirect('user.login');
+    }
+
+    /* @var \League\OAuth2\Client\Provider\GoogleUser|null $profile */
+    $profile = $this->processCallback();
+
+    // If authentication was successful.
+    if ($profile !== NULL) {
+
+      // Gets (or not) extra initial data.
+      $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
+
+      return $this->userAuthenticator->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->getAvatar(), $data);
+    }
+
+    return $this->redirect('user.login');
+  }
+
+}
---
+++ web/modules/contrib/social_auth_google/src/Plugin/Network/GoogleAuthInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\social_auth_google\Plugin\Network;
+
+use Drupal\social_auth\Plugin\Network\NetworkInterface;
+
+/**
+ * Defines the Google Auth interface.
+ */
+interface GoogleAuthInterface extends NetworkInterface {}
