1514d8db530779a2b9accaed457f8251dae1acce
mcdwayne <1dwayne.mcdaniel@gmail.com>
2019-05-18 21:07:34 -0700

    added modulename to this branch

---
+++ web/modules/contrib/converter_tools/src/Form/ConverterToolsOnlyLettersForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\converter_tools\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Form for get only letters from text.
+ */
+class ConverterToolsOnlyLettersForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'converter_tools_only_letters';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['converter_tools_text'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Text'),
+      '#rows' => 20,
+      '#cols' => 100,
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Convert'),
+    ];
+
+    $form['converter_tools_result'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Result'),
+      '#attributes' => ['readonly' => 'readonly'],
+      '#disabled' => TRUE,
+      '#rows' => 20,
+      '#cols' => 100,
+    ];
+
+    if ($form_state->isRebuilding() && !empty($form_state->getValue('converter_tools_result'))) {
+
+      $result = $form_state->getValue('converter_tools_result');
+
+      $form['converter_tools_result']['#value'] = $result;
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $text = $form_state->getValue('converter_tools_text');
+
+    $result = preg_replace('/[0-9]+/', '', $text);
+
+    $result = preg_replace('/[^\p{L}\p{N}\s]/u', '', $result);
+
+    $result = str_replace('ˆ', '', $result);
+
+    $form_state->setValue('converter_tools_result', $result);
+
+    $form_state->setRebuild();
+
+  }
+
+}
